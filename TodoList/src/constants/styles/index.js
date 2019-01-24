0533c81fda86192acc1bb0a7d786b0c2dbdd2e1b
Aula Radix <radix@SJ208F20.local>
2019-01-24 11:44:11 -0200

    correcao do style

--- TodoList/src/constants/styles/index.js
+++ TodoList/src/constants/styles/index.js
@@ -1,4 +1,4 @@
-import {StyleSheet} from 'react-native';
+import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 container: {
@@ -19,4 +19,4 @@ instructions: {
 },
 });
 
-export default {styles};
+export default styles;
--- TodoList/src/screens/HomeScreen/index.js
+++ TodoList/src/screens/HomeScreen/index.js
@@ -1,16 +1,14 @@
 import React, {Component} from 'react';
 import {View, Text, SafeAreaView} from 'react-native';
-import {styles} from '../../constants/styles';
+import styles from '../../constants/styles';
 
 export default class HomeScreen extends Component {
     render() {
         return(
-            <SafeAreaView>                
-            {/* <View style={styles.container}>
-                <Text style={styles.welcome}>Welcome to React Native!</Text>
-                <Text style={styles.instructions}>To get started, edit App.js</Text>
-                <Text style={styles.instructions}></Text>
-            </View> */}
+            <SafeAreaView style={styles.container}>           
+                    <Text style={styles.welcome}>Welcome to React Native!</Text>
+                    <Text style={styles.instructions}>To get started, edit App.js</Text>
+                    <Text style={styles.instructions}></Text>
             </SafeAreaView>
         )
     }
